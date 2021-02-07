d38766e8a602fcd1ae4961ea55c0eca478faa71e
Florian Uekermann <florian@uekermann.me>
2021-02-07 17:11:23 +0100

    add docs and other details

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,16 +1,21 @@
 use crate::acme::{Account, AcmeError, Auth, Directory, Identifier, Order, ACME_TLS_ALPN_NAME};
+use crate::persist::{read_if_exist, write};
 use async_rustls::rustls::sign::{any_ecdsa_type, CertifiedKey};
 use async_rustls::rustls::Certificate as RustlsCertificate;
 use async_rustls::rustls::{ClientHello, PrivateKey, ResolvesServerCert};
+use async_std::path::Path;
 use async_std::task::sleep;
+use chrono::Utc;
 use futures::future::try_join_all;
 use pem::PemError;
 use rcgen::{CertificateParams, DistinguishedName, RcgenError, PKCS_ECDSA_P256_SHA256};
+use ring::digest::{Context, SHA256};
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
 use thiserror::Error;
+use x509_parser::parse_x509_certificate;
 
 pub struct ResolvesServerCertUsingAcme {
     cert_key: Mutex<Option<CertifiedKey>>,
@@ -24,16 +29,44 @@ impl ResolvesServerCertUsingAcme {
             auth_keys: Mutex::new(BTreeMap::new()),
         })
     }
-    pub async fn run(&self, directory_url: impl AsRef<str>, domains: Vec<String>) {
-        match self.order(directory_url, &domains).await {
-            Ok(_) => log::info!("successfully ordered certificate"),
-            Err(err) => log::error!("ordering certificate failed: {}", err),
-        };
+    pub async fn run<P: AsRef<Path>>(
+        &self,
+        directory_url: impl AsRef<str>,
+        mut domains: Vec<String>,
+        cache_dir: Option<P>,
+    ) {
+        domains.sort();
+        let file_name = Self::cached_cert_file_name(&domains);
+        self.load_certified_key(&cache_dir, &file_name).await;
+
+        let mut err_cnt = 0usize;
+        loop {
+            let d = self.duration_until_renewal_attempt(err_cnt);
+            if d.as_secs() != 0 {
+                log::info!("next renewal attemt in {}s", d.as_secs());
+                sleep(d).await;
+            }
+            match self
+                .order(&directory_url, &domains, &cache_dir, &file_name)
+                .await
+            {
+                Ok(_) => {
+                    log::info!("successfully ordered certificate");
+                    err_cnt = 0;
+                }
+                Err(err) => {
+                    log::error!("ordering certificate failed: {}", err);
+                    err_cnt += 1;
+                }
+            };
+        }
     }
-    async fn order(
+    async fn order<P: AsRef<Path>>(
         &self,
         directory_url: impl AsRef<str>,
         domains: &Vec<String>,
+        cache_dir: &Option<P>,
+        file_name: &str,
     ) -> Result<(), OrderError> {
         let mut params = CertificateParams::new(domains.clone());
         params.distinguished_name = DistinguishedName::new();
@@ -63,16 +96,102 @@ impl ResolvesServerCertUsingAcme {
                 }
                 Order::Valid { certificate } => {
                     log::info!("download certificate");
-                    let cert = account.certificate(certificate).await?;
-                    let cert = pem::parse(cert)?.contents;
-                    let cert_key = CertifiedKey::new(vec![RustlsCertificate(cert)], Arc::new(pk));
-                    self.cert_key.lock().unwrap().replace(cert_key);
-                    log::info!("Done");
+                    let acme_cert_pem = account.certificate(certificate).await?;
+                    let acme_cert = RustlsCertificate(pem::parse(&acme_cert_pem)?.contents);
+                    let cert_key = CertifiedKey::new(vec![acme_cert], Arc::new(pk));
+                    self.cert_key.lock().unwrap().replace(cert_key.clone());
+                    let pk_pem = cert.serialize_private_key_pem();
+                    Self::save_certified_key(cache_dir, file_name, pk_pem, acme_cert_pem).await;
                     return Ok(());
                 }
                 Order::Invalid => return Err(OrderError::BadOrder(order)),
             }
         }
+    }
+    fn duration_until_renewal_attempt(&self, err_cnt: usize) -> Duration {
+        let valid_until = match self.cert_key.lock().unwrap().clone() {
+            None => 0,
+            Some(cert_key) => match cert_key.cert.first() {
+                Some(cert) => match parse_x509_certificate(cert.0.as_slice()) {
+                    Ok((_, cert)) => cert.validity().not_after.timestamp(),
+                    Err(err) => {
+                        log::error!("could not parse certificate: {}", err);
+                        0
+                    }
+                },
+                None => 0,
+            },
+        };
+        let valid_secs = (valid_until - Utc::now().timestamp()).max(0);
+        let wait_secs = Duration::from_secs(valid_secs as u64 / 2);
+        match err_cnt {
+            0 => wait_secs,
+            err_cnt => wait_secs.max(Duration::from_secs(1 << err_cnt)),
+        }
+    }
+    async fn load_certified_key<P: AsRef<Path>>(
+        &self,
+        cache_dir: &Option<P>,
+        cert_cache_name: &str,
+    ) {
+        let mut pems = match cache_dir {
+            Some(cache_dir) => match read_if_exist(cache_dir, &cert_cache_name).await {
+                Ok(content) => match content {
+                    Some(content) => pem::parse_many(content),
+                    None => return,
+                },
+                Err(err) => {
+                    log::error!("could not read certificate from cache directory: {}", err);
+                    return;
+                }
+            },
+            None => return,
+        };
+        if pems.len() < 2 {
+            log::error!(
+                "expected 2 or more pem in {}, got: {}",
+                cert_cache_name,
+                pems.len()
+            );
+            return;
+        }
+        let pk = match any_ecdsa_type(&PrivateKey(pems.remove(0).contents)) {
+            Ok(pk) => pk,
+            Err(_) => {
+                log::error!("{} does not contain an ecdsa private key", cert_cache_name);
+                return;
+            }
+        };
+        let acme_cert = RustlsCertificate(pems.remove(0).contents);
+        let cert_key = CertifiedKey::new(vec![acme_cert], Arc::new(pk));
+        self.cert_key.lock().unwrap().replace(cert_key);
+        log::info!("found certificate in cache directory")
+    }
+    async fn save_certified_key<P: AsRef<Path>>(
+        cache_dir: &Option<P>,
+        cert_cache_name: &str,
+        pk_pem: String,
+        acme_cert_pem: String,
+    ) {
+        match cache_dir {
+            Some(cache_dir) => {
+                let content = format!("{}\n{}", pk_pem, acme_cert_pem);
+                match write(cache_dir, cert_cache_name, &content).await {
+                    Ok(_) => log::info!("saved certificate in cache directory"),
+                    Err(err) => log::error!("could not save certificate: {}", err),
+                }
+            }
+            None => log::info!("could not save certificate, no cache directory specified"),
+        }
+    }
+    fn cached_cert_file_name(domains: &Vec<String>) -> String {
+        let mut ctx = Context::new(&SHA256);
+        for domain in domains {
+            ctx.update(domain.as_ref());
+            ctx.update(&[0])
+        }
+        let hash = base64::encode_config(ctx.finish(), base64::URL_SAFE_NO_PAD);
+        format!("cached_cert_{}", hash)
     }
     async fn authorize(&self, account: &Account, url: &String) -> Result<(), OrderError> {
         let (domain, challenge_url) = match account.auth(url).await? {
